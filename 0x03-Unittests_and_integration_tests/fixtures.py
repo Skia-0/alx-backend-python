f1b65ce887580e926970f57b7caf3709067493c9
Courage Kemawor <kemaworc00@gmail.com>
2025-11-16 21:07:16 +0000

    Update fixtures.py

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,4 +1,15 @@
-org_payload = {"login": "google", "repos_url": "http://fake.url/repos"}
-repos_payload = [{"name": "repo1", "license": {"key": "my_license"}}]
-expected_repos = ["repo1"]
-apache2_repos = []
+# fixtures.py
+
+org_payload = {
+    "login": "holberton",
+    "id": 1,
+    "repos_url": "https://api.github.com/orgs/holberton/repos"
+}
+
+repos_payload = [
+    {"name": "repo1", "license": {"key": "apache-2.0"}},
+    {"name": "repo2", "license": {"key": "mit"}}
+]
+
+expected_repos = ["repo1", "repo2"]
+apache2_repos = ["repo1"]  # Only repos with license key apache-2.0
