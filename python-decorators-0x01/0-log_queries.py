c677f30ffa8de195958610c118764dda9e039fc2
Courage Kemawor <kemaworc00@gmail.com>
2025-11-16 19:09:59 +0000

    Update 0-log_queries.py

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 import sqlite3
 import functools
+from datetime import datetime
 
-# -----------------------------
+# -----------------------------------
 # log_queries decorator
-# -----------------------------
+# -----------------------------------
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         query = kwargs.get("query", None)
         if query:
-            print(f"Executing query: {query}")
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"[{timestamp}] Executing query: {query}")
         return func(*args, **kwargs)
     return wrapper
 
@@ -25,7 +27,8 @@ def fetch_all_users(query):
     return results
 
 
-# Example usage
+# Execute if run directly
 if __name__ == "__main__":
     users = fetch_all_users(query="SELECT * FROM users")
     print(users)
+
