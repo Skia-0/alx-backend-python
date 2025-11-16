c6140b749edec26c638ede606388a26c15de0ef5
Courage Kemawor <kemaworc00@gmail.com>
2025-11-16 21:02:34 +0000

    Create fixtures.py

---
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -0,0 +1,4 @@
+org_payload = {"login": "google", "repos_url": "http://fake.url/repos"}
+repos_payload = [{"name": "repo1", "license": {"key": "my_license"}}]
+expected_repos = ["repo1"]
+apache2_repos = []
